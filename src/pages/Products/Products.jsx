c0a28cf20ea6c67a7da822ac74f1368314bfdb35
shraddha319 <shraddha.jain3219@gmail.com>
2021-05-15 03:38:32 +0530

    style: imported stylesheet

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,24 @@
-import { useEffect } from "react";
-import { useProducts } from "../../contexts/Products.context";
+import { useEffect, useState } from "react";
+import { useProducts } from "../../contexts/index";
+import { getProducts } from "../../api/index";
+import { Loader } from "../../components/index";
+import ProductCard from "./ProductCard";
+import "./Products.css";
 
 export default function Products() {
-  const { products, disptachProducts } = useProducts();
+  const { products, dispatchProducts } = useProducts();
+  const [enableLoader, setEnableLoader] = useState(false);
 
-  return <div className="Products"></div>;
+  useEffect(() => getProducts(dispatchProducts, setEnableLoader), []);
+
+  return (
+    <div className="Products">
+      {enableLoader ? <Loader /> : null}
+      <div className="products__list">
+        {products.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
