55b693184255fa83d986e97fd05c4ef294aa24e0
shraddha319 <shraddha.jain3219@gmail.com>
2021-05-27 17:07:42 +0530

    refactor: created button component

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,16 @@ import Navbar from "./shared/Navbar/Navbar";
 import Loader from "./shared/Loader/Loader";
 import CardPrice from "./shared/CardPrice/CardPrice";
 import CardRating from "./shared/CardRating/CardRating";
+import {
+    ButtonWishlist,
+    ButtonCart
+} from "./Buttons/Buttons"
 
 export {
     Loader,
     Navbar,
     CardPrice,
-    CardRating
+    CardRating,
+    ButtonWishlist,
+    ButtonCart
 }
--- src/pages/Products/ProductCard.jsx
+++ src/pages/Products/ProductCard.jsx
@@ -1,89 +1,29 @@
 import "./ProductCard.css";
-import { addToCart, addToWishlist } from "../../api/index";
-import { Link } from "react-router-dom";
+import {
+  ButtonWishlist,
+  ButtonCart,
+  CardPrice,
+  CardRating,
+} from "../../components/index";
 
-export default function ProductCard({
-  product,
-  userData,
-  dispatchUserData,
-  isProductInCart,
-  isProductInWishlist,
-}) {
+export default function ProductCard({ product }) {
   return (
-    <div className="card card--product clickable hover--scale-out">
-      {isProductInWishlist(product._id) ? (
-        <Link to="/wishlist">
-          <button className="btn btn--icon">
-            <span className="fa--sm fa--hover fa--primary">
-              <i className="fas fa-heart"></i>
-            </span>
-          </button>
-        </Link>
-      ) : (
-        <button
-          className="btn btn--icon"
-          onClick={() => addToWishlist(product._id, userData, dispatchUserData)}
-        >
-          <span className="fa--sm fa--hover fa--primary">
-            <i className="far fa-heart"></i>
-          </span>
-        </button>
-      )}
-      <div className="card__header"></div>
+    <div className="card card--product hover--scale-out">
+      <div className="card__header">
+        <ButtonWishlist productId={product._id} />
+      </div>
       <div className="card__media">
         <img className="card__media--image" src={product.bookCoverURL} />
       </div>
       <div className="card__body flex--column">
         <p className="text--sm">{product.name}</p>
         <h2 className="text--muted">{`By ${product.author}`}</h2>
-        <ProductRating rating={product.rating} />
-        <ProductPrice price={product.price} />
+        <CardRating rating={product.rating} />
+        <CardPrice price={product.price} />
       </div>
-      <div className="card__footer justify-center">
-        {isProductInCart(product._id) ? (
-          <Link className="link" to="/cart">
-            <button className="btn btn--icon--left text--xs btn--secondary">
-              <span className="btn__icon fa--xs">
-                <i className="fas fa-shopping-cart"></i>
-              </span>
-              <p className="btn__text">GO TO CART</p>
-            </button>
-          </Link>
-        ) : (
-          <button
-            onClick={() => addToCart(product._id, userData, dispatchUserData)}
-            className="btn btn--icon--left text--xs btn--secondary"
-          >
-            <span className="btn__icon fa--xs">
-              <i className="fas fa-shopping-cart"></i>
-            </span>
-            <p className="btn__text">ADD TO CART</p>
-          </button>
-        )}
+      <div className="card__footer justify-between">
+        <ButtonCart productId={product._id} />
       </div>
     </div>
   );
-
-  function ProductPrice({ price: { net, MRP, discountPercent } }) {
-    return (
-      <div className="product__price flex--row">
-        <p className="text--sm">{`₹ ${net}`}</p>
-        <p>
-          <strike className="text--muted">{`₹ ${MRP}`}</strike>
-        </p>
-        <p className="text--sm">
-          <strong>{`${discountPercent}% off`}</strong>
-        </p>
-      </div>
-    );
-  }
-
-  function ProductRating({ rating: { avgRating, ratingCount } }) {
-    return (
-      <div className="product__rating flex--row">
-        <p className="text--xs rating--badge">{`★ ${avgRating} `}</p>
-        <p className="text--muted rating--count">({ratingCount})</p>
-      </div>
-    );
-  }
 }
