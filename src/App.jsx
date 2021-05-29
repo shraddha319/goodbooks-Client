01ce270e291cc4c02a4e846518b150b8d0515ed8
shraddha319 <shraddha.jain3219@gmail.com>
2021-05-30 01:32:39 +0530

    feat: added Login page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Navbar, Loader, Toast } from "./components/index";
-import { Home, Products, Cart, Wishlist, ProductDetail } from "./pages/index";
+import {
+  Home,
+  Products,
+  Cart,
+  Wishlist,
+  ProductDetail,
+  Login,
+} from "./pages/index";
 import { useFeedback } from "./contexts/index";
-import { useEffect } from "react";
 import "./App.css";
 
 export default function App() {
@@ -21,6 +27,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import ProductDetail from "./Products/ProductDetail";
 import Cart from "./Cart/Cart";
 import Wishlist from "./Wishlist/Wishlist";
 import Home from "./Home/Home";
+import Login from "./Login/Login";
 
 export {
     Home,
     Products,
     ProductDetail,
     Cart,
-    Wishlist
+    Wishlist,
+    Login
 }
