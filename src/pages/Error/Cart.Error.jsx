b8a980be18f0f210a8adadd6399468a1805c59d4
shraddha319 <shraddha.jain3219@gmail.com>
2021-05-30 01:31:50 +0530

    style: updated error pages for empty cart/wishlist

--- src/pages/Error/Cart.Error.jsx
+++ src/pages/Error/Cart.Error.jsx
@@ -1,10 +1,23 @@
 import emptyCart from "../../assets/cart/empty-cart.png";
 import "./Error.css";
+import { Link } from "react-router-dom";
 
 export default function EmptyCart() {
   return (
-    <div className="error error--empty-cart">
-      <img src={emptyCart} alt="empty cart" />
+    <div className="error">
+      <div className="error__container">
+        <img src={emptyCart} alt="empty cart" />
+        <p className="text--lg">Your cart is empty!</p>
+        <Link
+          to="/products"
+          className="btn btn--lg btn--icon--right btn--primary"
+        >
+          <p class="btn__text">Shop Now</p>
+          <span class="btn__icon fa--sm">
+            <i class="fas fa-shopping-bag"></i>
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/Error/Wishlist.Error.jsx
+++ src/pages/Error/Wishlist.Error.jsx
@@ -1,10 +1,23 @@
 import emptyWishlist from "../../assets/wishlist/empty-wishlist.png";
 import "./Error.css";
+import { Link } from "react-router-dom";
 
 export default function EmptyWishlist() {
   return (
-    <div className="error error--empty-wishlist">
-      <img src={emptyWishlist} alt="empty wishlist" />
+    <div className="error">
+      <div className="error__container">
+        <img src={emptyWishlist} alt="empty wishlist" />
+        <p className="text--lg">Your Wishlist is empty!</p>
+        <Link
+          to="/products"
+          className="btn btn--lg btn--icon--right btn--primary"
+        >
+          <p class="btn__text">Shop Now</p>
+          <span class="btn__icon fa--sm">
+            <i class="fas fa-shopping-bag"></i>
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
