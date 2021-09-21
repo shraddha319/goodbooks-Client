4903f5b7adabfa7db1425411b01c4057f31c2f17
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-21 21:20:25 +0530

    feat: use react icons

---
+++ src/assets/icons.js
@@ -0,0 +1,21 @@
+import {
+  IoCartOutline as CartOutline,
+  IoCartSharp as CartSolid,
+  IoHeartOutline as HeartOutline,
+  IoHeartSharp as HeartSolid,
+  IoArrowBack as ArrowBack,
+  IoAddCircleOutline as AddIcon,
+  IoRemoveCircleOutline as RemoveIcon,
+  IoTrashBinOutline as TrashBinIcon,
+} from 'react-icons/io5';
+
+export {
+  CartOutline,
+  CartSolid,
+  HeartOutline,
+  HeartSolid,
+  ArrowBack,
+  AddIcon,
+  RemoveIcon,
+  TrashBinIcon,
+};
