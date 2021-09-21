54a1503dc0a686ba0172273557c2fd9268ad0d2e
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-21 21:34:04 +0530

    add validation constants

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const formError = {
+  EMAIL_UNIQUE: 'Email already exists.',
+  EMAIL_INVALID: 'Please enter valid email.',
+  PASSWORD_LEN: 'Password must be atleast 8 characters long.',
+  PASSWORD_INVALID:
+    'Password must contain at least one letter, one number and one special character.',
+  PASSWORD_MATCH: 'Passwords do not match.',
+  FIELD_REQUIRED: 'This is a required field',
+  USERNAME_EMAIL_INVALID: 'Please enter valid email or password',
+  LETTERS_ONLY: 'Alphabets only.',
+};
