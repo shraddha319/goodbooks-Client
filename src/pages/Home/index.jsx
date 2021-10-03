8f93503d9875d3d6d12d71ee82900ac278dea652
shraddha319 <shraddha.jain3219@gmail.com>
2021-10-03 22:15:03 +0530

    fix: remove login/signup links if authenticated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import './Home.css';
 import { Link } from 'react-router-dom';
 import Typewriter from 'typewriter-effect';
+import { useAuth } from '../../contexts';
 
 export default function Home() {
+  const {
+    auth: { token },
+  } = useAuth();
+
   return (
     <div className="Home">
       <section className="hero">
@@ -38,27 +43,41 @@ export default function Home() {
             <p className="text text--md">
               Buy Amazing Books at Amazing Prices!
             </p>
-            <Link
-              className="btn btn--md btn--icon--right btn--primary"
-              to="signup"
-            >
-              <p class="btn__text">Sign Up</p>
-              <span class="btn__icon fa--xs">
-                <i class="fas fa-user-plus"></i>
-              </span>
-            </Link>
-            <div className="hero__login flex-column">
-              <p className="text--sm">Already a member?</p>
+            {token ? (
               <Link
                 className="btn btn--md btn--icon--right btn--primary"
-                to="/login"
+                to="/books"
               >
-                <p class="btn__text">Login</p>
+                <p class="btn__text">Shop Now</p>
                 <span class="btn__icon fa--xs">
-                  <i class="fas fa-sign-in-alt"></i>
+                  <i class="fas fa-arrow-right"></i>
                 </span>
               </Link>
-            </div>
+            ) : (
+              <>
+                <Link
+                  className="btn btn--md btn--icon--right btn--primary"
+                  to="signup"
+                >
+                  <p class="btn__text">Sign Up</p>
+                  <span class="btn__icon fa--xs">
+                    <i class="fas fa-user-plus"></i>
+                  </span>
+                </Link>
+                <div className="hero__login flex-column">
+                  <p className="text--sm">Already a member?</p>
+                  <Link
+                    className="btn btn--md btn--icon--right btn--primary"
+                    to="/login"
+                  >
+                    <p class="btn__text">Login</p>
+                    <span class="btn__icon fa--xs">
+                      <i class="fas fa-sign-in-alt"></i>
+                    </span>
+                  </Link>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </section>
